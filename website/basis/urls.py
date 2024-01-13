22279a60c5586e0d8f49b56555b656fe143235ef
Kepler <keplerpocket@proton.me>
2024-01-13 20:41:22 +0100

    17. getenv function has been updated

--- website/basis/urls.py
+++ website/basis/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name="home"),
-    path('about/', views.AboutPage.as_view(), name='about'),
     path('contact/', views.ContactPage.as_view(), name='contact'),
+    path('about/', views.AboutPage.as_view(), name='about'),
     path('post/<slug:post_slug>/', views.PostPage.as_view(), name='post'),
     path('category/<slug:category_slug>/', views.CategoryPage.as_view(), name='category'),
     path('terms/', views.TermsPage.as_view(), name='terms'),
--- website/website/settings.py
+++ website/website/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from os import getenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m(v)&#27hid)gj23$s7(@z9c8!1fpsf$are@yg5(iwn#)zj#n&'
+SECRET_KEY = str(getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
