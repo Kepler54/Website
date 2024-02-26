822e9b486a314184183a544fd6f99cb1461f79a6
Kepler <126917322+Kepler54@users.noreply.github.com>
2024-02-26 14:49:38 +0100

    Update urls.py

--- website/basis/urls.py
+++ website/basis/urls.py
@@ -1,13 +1,15 @@
 from . import views
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-    path('', views.HomePage.as_view(), name="home"),
+    path('', cache_page(60)(views.HomePage.as_view()), name="home"),
     path('contact/', views.ContactPage.as_view(), name='contact'),
-    path('about/', views.AboutPage.as_view(), name='about'),
-    path('post/<slug:post_slug>/', views.PostPage.as_view(), name='post'),
-    path('category/<slug:category_slug>/', views.CategoryPage.as_view(), name='category'),
-    path('terms/', views.TermsPage.as_view(), name='terms'),
-    path('privacy/', views.PrivacyPage.as_view(), name='privacy'),
+    path('about/', cache_page(60)(views.AboutPage.as_view()), name='about'),
+    path('post/<slug:post_slug>/', cache_page(60)(views.PostPage.as_view()), name='post'),
+    path('category/<slug:category_slug>/', cache_page(60)(views.CategoryPage.as_view()), name='category'),
+    path('terms/', cache_page(60)(views.TermsPage.as_view()), name='terms'),
+    path('privacy/', cache_page(60)(views.PrivacyPage.as_view()), name='privacy'),
     path('email_was_sent/', views.email_was_sent, name='email_was_sent'),
+    path('error_404/', views.page_not_found, name='error_404'),
 ]
